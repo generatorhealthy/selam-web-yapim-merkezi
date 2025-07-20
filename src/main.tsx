1ddeaea70285d9c628a2f500f1f1514c63e64878
generatorhealthy <doktorumolcomtr@gmail.com>
2025-07-20 06:59:55 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { createRoot } from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <HelmetProvider>
     <App />
   </HelmetProvider>
